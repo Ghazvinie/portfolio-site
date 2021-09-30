eff20af6d4bc068757ab7ad4460e13afa8ef0d35
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-09-30 20:32:23 +0100

    working on navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,59 +1,81 @@
 import React from 'react';
-import { Link, animateScroll as scroll } from 'react-scroll';
+import { Link } from 'react-scroll';
+import { SocialIcon } from 'react-social-icons';
 
 export default function Nav() {
     return (
-        <nav className='nav'>
-            <div className='nav-content'>
-                <ul className="nav-items">
-                    <li className="nav-item">
+        <header className='bg-pink-100 bg-opacity-90' >
+            <div className='container mx-auto flex justify-between'>
+                <nav className='flex'>
+                    <Link
+                        className='nav-item inline-flex items-center py-3 px-3 mr-4 cursor-pointer font-medium text-4xl'
+                        activeClass='underline'
+                        to='about'
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={500}
+                    >About
+                    </Link>
+                    <Link
+                        className='nav-item inline-flex items-center py-3 px-3 mr-4 cursor-pointer font-medium text-4xl'
+                        activeClass='underline'
+                        to='skills'
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={500}
+                    >Skills
+                    </Link>
+                    <Link
+                        className='nav-item inline-flex items-center py-3 px-3 mr-4 cursor-pointer font-medium text-4xl'
+                        activeClass='underline'
+                        to='projects'
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={500}
+                    >Projects
+                    </Link>
+                    <Link
+                        className='nav-item inline-flex items-center py-3 px-3 mr-4 cursor-pointer font-medium text-4xl'
+                        activeClass='underline'
+                        to='contact'
+                        spy={true}
+                        smooth={true}
+                        offset={-50}
+                        duration={500}
+                    >Contact
+                    </Link>
+                    {/* <li className='nav-item inline-flex items-end py-1 ml-80 mr-20 cursor-pointer font-medium text-xs '>
                         <Link
-                            activeClass="active"
-                            to="about"
+                            activeClass='underline'
+                            to='bonus'
                             spy={true}
                             smooth={true}
-                            offset={-70}
+                            offset={-50}
                             duration={500}
-                        >
-                            About
-                        </Link></li>
-                        <li className="nav-item">
-                        <Link
-                            activeClass="active"
-                            to="skills"
-                            spy={true}
-                            smooth={true}
-                            offset={-70}
-                            duration={500}
-                        >
-                            Skills
-                        </Link></li>
-                        <li className="nav-item">
-                        <Link
-                            activeClass="active"
-                            to="projects"
-                            spy={true}
-                            smooth={true}
-                            offset={-70}
-                            duration={500}
-                        >
-                            Projects
-                        </Link></li>
-                        <li className="nav-item">
-                        <Link
-                            activeClass="active"
-                            to="contact"
-                            spy={true}
-                            smooth={true}
-                            offset={-70}
-                            duration={500}
-                        >
-                            Contact
-                        </Link></li>
-                </ul>
+                        >Bonus Photos
+                        </Link></li> */}
 
+
+                </nav>
+                <div >
+                    <SocialIcon
+                        url='https://github.com/Ghazvinie'
+                        bgColor='black'
+                        className='mr-4 mt-7'
+                        target='_blank'
+                        style={{ height: 30, width: 30 }} />
+                    <SocialIcon
+                        url='https://www.linkedin.com/in/daniel-ghazvinie-53a304188/'
+                        bgColor='black'
+                        target='_blank'
+                        className='mr-4 mt-7'
+                        style={{ height: 30, width: 30 }} />
+                </div>
             </div>
 
-        </nav>
-    )
+        </header>
+    );
 }
