af8b5336e96c0dee5f59963ddc690e1c04ae0304
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-10-06 18:35:39 +0100

    added email link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-scroll';
 import { SocialIcon } from 'react-social-icons';
+import { MdEmail } from 'react-icons/md';
 
 export default function Nav() {
     return (
@@ -51,31 +52,41 @@ export default function Nav() {
 
 
                 </nav>
-                <div className='ml-auto flex-column'>
-                    <div className='flex'>
-                    <SocialIcon
-                        url='https://github.com/Ghazvinie'
-                        bgColor='black'
-                        className='mr-1 ml-1 mt-4'
-                        target='_blank'
-                        style={{ height: 30, width: 30 }} />
-                    <SocialIcon
-                        url='https://www.linkedin.com/in/daniel-ghazvinie-53a304188/'
-                        bgColor='black'
-                        target='_blank'
-                        className='mr-1 ml-1 mt-4'
-                        style={{ height: 30, width: 30 }} />
+                <div className='flex flex-col justify-center items-center self-end'>
+                    <div className='flex flex-row mt-auto items-center justify-center'>
+                        <SocialIcon
+                            url='https://github.com/Ghazvinie'
+                            bgColor='black'
+                            className='mx-1'
+                            target='_blank'
+                            style={{ height: 30, width: 30 }} />
+                        <SocialIcon
+                            url='https://www.linkedin.com/in/daniel-ghazvinie-53a304188/'
+                            bgColor='black'
+                            target='_blank'
+                            className='mx-1'
+                            style={{ height: 30, width: 30 }} />
+                        <a href="mailto:danielghazvinie@protonmail.com">
+                            <MdEmail
+                                bgColor='black'
+                                target='_blank'
+                                className='mx-1'
+                                style={{ height: 32, width: 32 }} />
+                        </a>
                     </div>
-                    <Link
-                        className='cursor-pointer font-medium text-xs inline-flex text-center text-white hover:text-black'
-                        activeClass='underline'
-                        to='bonus'
-                        spy={true}
-                        smooth={true}
-                        offset={-50}
-                        duration={500}
-                    >Bonus Photos
-                    </Link>
+                    <div>
+                        <Link
+                            className='cursor-pointer font-medium text-xs text-right text-white hover:text-black w-full'
+                            activeClass='underline'
+                            to='bonus'
+                            spy={true}
+                            smooth={true}
+                            offset={-50}
+                            duration={500}
+                        >Bonus Photos
+                        </Link>
+                    </div>
+
                 </div>
 
             </div>
