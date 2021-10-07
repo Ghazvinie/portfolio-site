e827499ea18fa892aabfc7bf0a7b53c808cf6066
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-10-07 08:08:46 +0100

    added flashing arrows

--- src/components/About.js
+++ src/components/About.js
@@ -9,20 +9,26 @@ export default function About() {
             name='about'
         >
             <h1 className='text-4xl font-semibold'>About</h1>
-            <div className='text-3xl leading-loose ml-36 justify-center md:h-screen'>
-                Hello again.
-                <br />
-                I am a bespoke tailor turned passionate developer.
-                <br />
-                I love to code and work with computers!
-                <br />
-                I have experience creating real world projects and working on real-world codebases.
-                <br />
-                Please scroll down to view my skills and some of my projects...
-                <br /> <br /> 
-               
-                <FlashingArrows />
+            <div className=' ml-36 justify-center md:h-screen'>
+                <div className='text-3xl leading-loose text-start mb-10'>
+                    Hello again.
+                    <br />
+                    I am a bespoke tailor turned passionate developer.
+                    <br />
+                    I love to code and work with computers!
+                    <br />
+                    I have experience creating real world projects and working on real-world codebases.
+                    <br />
+                    Please scroll down to view my skills and some of my projects...
+                    <br />
+                </div>
+
+
+                <div className='text-3xl md:text-5xl flex flex-col w-min mb-10'>
+                    <FlashingArrows />
+                </div>
             </div>
+
             <p className='ml-36 text-base font-extralight'>(Yes, the website is black and white)</p>
         </div>
     );
--- src/components/FlashingArrows.js
+++ src/components/FlashingArrows.js
@@ -1,23 +1,48 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { IoIosArrowDown } from 'react-icons/io';
 
 export default function FlashingArrows() {
-const [index, setIndex] = useState(0);
- 
+    const [index, setIndex] = useState(0);
+    const [arrows, setArrows] = useState([
+        <IoIosArrowDown style={{ color: 'rgba(128, 128, 128, 0.329)' }} className='my-1'/>,
+        <IoIosArrowDown className='my-1'/>,
+        <IoIosArrowDown className='my-1'/>,
+        <IoIosArrowDown className='my-1'/>,
+        <IoIosArrowDown className='my-1'/>,
+        <IoIosArrowDown className='my-1'/>,
+        <IoIosArrowDown className='my-1'/>,
+        <IoIosArrowDown className='my-1'/>,
+        <IoIosArrowDown className='my-1'/>,
+    ])
+
     const offStyle = {
         color: 'white'
     }
+
+    useEffect(() => {
+        index > arrows.length - 1 && setIndex(0)
+        const move = (from, to) => {
+            var temp = arrows.splice(from, 1)[0]
+            arrows.splice(to, 0, temp)
+        };
+
+        const timer = setInterval(() => {
+            if (index === arrows.length - 1) {
+                move(index, 0)
+                setIndex(0)
+                return
+            }
+            move(index, index + 1);
+            setIndex((prevIndex) => prevIndex + 1);
+        }, 80);
+        return () => clearInterval(timer);
+    });
+
     return (
-        <div>
-            <IoIosArrowDown />
-            <IoIosArrowDown />
-            <IoIosArrowDown style={offStyle} />
-            <IoIosArrowDown />
-            <IoIosArrowDown />
-            <IoIosArrowDown />
-            <IoIosArrowDown />
-            <IoIosArrowDown />
-            <IoIosArrowDown />
-        </div>
-    )
+        <>
+            {
+                arrows.map(element => element)
+            }
+        </>
+    );
 }
