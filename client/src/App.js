f0f8128da8aac2e511d6df9d4572399644bca0cc
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-10-17 16:33:13 +0100

    adding server

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css'
 import Nav from './components/Nav';
 import Welcome from './components/Welcome';
@@ -8,8 +9,18 @@ import Contact from './components/Contact';
 import Photos from './components/Photos';
 
 function App() {
+
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
   return (
     <div className='app flex flex-col'>
+      {data}
       <Nav/>
       <Welcome/>
       <About />
