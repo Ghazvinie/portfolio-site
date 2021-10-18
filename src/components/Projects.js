3f3492e2e400b50381b4a8cb6fff758c502951e4
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-10-18 17:48:13 +0100

    adding stack to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,14 +6,14 @@ import { BiLinkExternal } from 'react-icons/bi';
 export default function Projects() {
     return (
         <div
-            className='flex flex-col border-black border-t-4 m-2 p-2 md:h-screen'
+            className='flex flex-col border-black border-t-4 m-2 p-2 '
             name='projects'
         >
             <h1 className='text-4xl font-semibold'>Projects</h1>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 ml-18 md:ml-36 justify-items-center items-center' >
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 ml-18 md:ml-36 justify-items-center items-center' >
                 {
                     projects.map(project => (
-                        <div className='flex flex-col ml-2'>
+                        <div className='flex flex-col ml-2 border-black border-b-2'>
                             <img src={project.img} className='w-full h-48 object-scale-down text-center' alt={project.title} />
                             <h1 className='text-center text-lg font-bold break-word mb-3 mt-4'>
                                 {project.title}
@@ -21,6 +21,11 @@ export default function Projects() {
                             <p className='break-word text-center text-medium font-light m-2 text-center'>
                                 {project.description}
                             </p>
+                            <div className='flex flex-row justify-center m-2'>
+                                {
+                                    project.stack?.map(tech => <tech.icon style={{ width: 25, height: 25 }} className='m-1' title={tech.text}/>)
+                                }
+                            </div>
                             <div className='flex items-center justify-center flex-row m-2'>
                                 <a
                                     href={`${project.github}`}
--- src/data/projects.js
+++ src/data/projects.js
@@ -18,56 +18,56 @@ import { ReactComponent as expressjsIcon } from '../assets/expressjs.svg';
 const projects = [
     {
         title: 'Custom SpeechKit Audio Player',
-        description : 'Work experience project - developed a custom audio player based on SpeechKit\'s PlayerSDK',
-        stack: [SiHtml5, SiCss3, SiJavascript, SiReact],
+        description: 'Work experience project - developed a custom audio player based on SpeechKit\'s PlayserSDK and a guide on how this can be done.',
+        stack: [{ icon: SiHtml5, text: 'HTML' }, { icon: SiCss3, text: 'CSS' }, { icon: SiJavascript, text: 'JavaScript' }, { icon: SiReact, text: 'ReactJS' }],
         github: 'https://github.com/Ghazvinie/custom-speechkit-audio-player',
         live: 'ff',
         img: player
     },
     {
         title: 'SpeechKit RSS Validator',
-        description : 'Full Stack work experience project - this involved creating an RSS feed validator for clients to check their feeds are formatted correctly and include all necessary data.',
-        stack: [SiHtml5, SiCss3, SiJavascript, SiReact, SiNodeDotJs, expressjsIcon],
+        description: 'Work experience project - this involved creating an RSS feed validator for clients to check their feeds are formatted correctly and include all necessary data.',
+        stack: [{ icon: SiHtml5, text: 'HTML' }, { icon: SiCss3, text: 'CSS' }, { icon: SiJavascript, text: 'JavaScript' }, { icon: SiReact, text: 'ReactJS' }, { icon: SiNodeDotJs, text: 'NodeJs' }, { icon: expressjsIcon, text: 'ExpressJs' }],
         github: 'https://github.com/Ghazvinie/rss_validator',
         live: 'https://speechkit-rss-validator.herokuapp.com/',
         img: rss
     },
     {
         title: 'Express Error Handler',
-        description : 'Some examples of how errors can be handled in an ExpressJs application.',
+        description: 'Some examples of how errors can be handled in an ExpressJs application.',
         github: 'https://github.com/Ghazvinie/express-error-handler',
         live: '',
         img: error
     },
     {
         title: 'OAuth 2.0 Sign Up and Sign In',
-        description : 'An example of user authentication and authorisation based on the OAuth protocol.',
+        description: 'An example of user authentication and authorisation based on the OAuth protocol.',
         github: 'https://github.com/Ghazvinie/user-auth-with-passport-oauth',
         live: '',
         img: oauth
     },
     {
         title: 'User Auth With JWT',
-        description : 'An example of user authentication and authorisation using JWT and CSRF tokens.',
+        description: 'An example of user authentication and authorisation using JWT and CSRF tokens.',
         github: 'https://github.com/Ghazvinie/user-auth-with-jwt',
         live: '',
         img: 'somepic'
     },
     {
         title: 'MongoDB CRUD App',
-        description : 'An extension of a FCC challenge to create an exercise tracking app.',
+        description: 'An extension of a FCC challenge to create an exercise tracking app.',
         github: 'https://github.com/Ghazvinie/mongodb-crud-exercise-tracker-app',
         live: '',
         img: 'somepic'
     },
     {
         title: 'Conway\'s Game of Life',
-        description : 'JavaScript implementation of Conway\'s Game of Life.',
+        description: 'JavaScript implementation of Conway\'s Game of Life.',
         github: 'https://github.com/Ghazvinie/conways-game-of-life',
         live: '',
         img: 'somepic'
     },
-    
+
 ]
 
 export default projects;
