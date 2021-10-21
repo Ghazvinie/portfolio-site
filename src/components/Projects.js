9e1c38ebd2bca8a79593b7570c2b67e1b0c73961
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-10-21 18:19:00 +0100

    fixing some small styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,15 +6,15 @@ import { BiLinkExternal } from 'react-icons/bi';
 export default function Projects() {
     return (
         <div
-            className='flex flex-col border-black border-t-4 m-2 p-2 '
+            className='flex flex-col border-black border-t-4 m-2 p-2 mb-10 '
             name='projects'
         >
-            <h1 className='text-4xl font-semibold'>Projects</h1>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-44 ml-18 md:ml-36 justify-items-center items-center' >
+            <h1 className='text-4xl font-semibold mb-10'>Projects</h1>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-24 ml-18 md:ml-36 justify-items-center items-center' >
                 {
                     projects.map(project => (
-                        <div className='grid auto-rows-min gap-0 ml-2 justify-center content-center border-2 border-black p-4'>
-                            <img src={project.img} className='w-11/12 h-80 object-scale-down text-center m-5 justify-center' alt={project.title} />
+                        <div className='grid auto-rows-min justify-items-center ml-2 border-2 border-black p-4'>
+                            <img src={project.img} className='w-11/12 h-80 object-scale-down text-center mb-3' alt={project.title} />
                             <h1 className='text-center text-lg font-bold break-word my-4'>
                                 {project.title}
                             </h1>
@@ -33,7 +33,7 @@ export default function Projects() {
                                     target='_blank'
                                     title='GitHub Link'
                                     className='mx-5'>
-                                    <FaGithubSquare style={{ width: 25, height: 25 }} />
+                                    <FaGithubSquare style={{ width: 25, height: 25 }} className='bg-grey' />
                                 </a>
                                 {
                                     project.live && <a
